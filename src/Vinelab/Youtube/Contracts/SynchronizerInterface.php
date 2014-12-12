5353916601a960291791b24fec2c9b8014d28da8
Mahmoud Zalt <mahmoud.z@msn.com>
2014-12-12 14:51:03 +0200

    fix the sync function

--- src/Vinelab/Youtube/Contracts/SynchronizerInterface.php
+++ src/Vinelab/Youtube/Contracts/SynchronizerInterface.php
@@ -1,18 +1,19 @@
 <?php namespace Vinelab\Youtube\Contracts;
 
 interface SynchronizerInterface {
-    
-   /**
-    * Sync the resources.
-    * 
-    * in case a resource(video, or channel) has been been deleted
-    * a 'IncompatibleParametersObjectTypesException' will be thrown
-    * which means that the existing data and the new one are not 
-    * compatible(different kind) because if the resource has been deleted
-    * Null will be returned in the response.
-    * 
-    * @param  Channel|Video $resource          
-    * @return Channel|Video
-    */
+
+    /**
+     * Sync the resources.
+     *
+     * in case a resource(video, or channel) has been been deleted
+     * a 'IncompatibleParametersObjectTypesException' will be thrown
+     * which means that the existing data and the new one are not
+     * compatible(different kind) because if the resource has been deleted
+     * Null will be returned in the response.
+     *
+     * @param \Vinelab\Youtube\Contracts\ResourceInterface $resource
+     *
+     * @return mixed
+     */
     public function sync($resource);
 }
