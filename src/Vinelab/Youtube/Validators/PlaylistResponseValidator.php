10f50fe9f3dbcf4748503468e8963b79064d4892
Mahmoud Zalt <mahmoud.z@msn.com>
2015-02-27 16:29:52 +0200

    validate playlist response

---
+++ src/Vinelab/Youtube/Validators/PlaylistResponseValidator.php
@@ -0,0 +1,38 @@
+<?php namespace Vinelab\Youtube\Validators;
+
+/**
+ * @author Mahmoud Zalt <mahmoud@vinelab.com>
+ */
+
+use Vinelab\Youtube\Exceptions\InvalidResponseException;
+
+class PlaylistResponseValidator extends Validator {
+
+    protected $rules = [
+        'kind'  =>  'required',
+        'etag'  =>  'required',
+    ];
+
+    public function validate($attributes)
+    {
+        // convert the StdObj to array
+        $attributes = $this->objectToArray($attributes);
+
+        /**
+         * check if the converted object has the same keys
+         * as the expected response from youtube.
+         * if not, throw InvalidResponseException.
+         */
+        if (! $this->expectedKeys($attributes, ExpectedYoutubeResponse::channel())) {
+            throw new InvalidResponseException();
+        }
+
+        $validation = $this->validation($attributes);
+
+        if ($validation->fails()) {
+            throw new InvalidResponseException($validation->messages()->all());
+        }
+
+        return true;
+    }
+}
