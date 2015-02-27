4bf12f1c9bdfd02c08e61853fd58484b31a14779
Mahmoud Zalt <mahmoud.z@msn.com>
2015-02-27 16:29:09 +0200

    parse the playlist response
    make the `parse` function accept different type of inputs to be parsed,
    in our case we can parse `channel` or `playlist`.

--- src/Vinelab/Youtube/Parser.php
+++ src/Vinelab/Youtube/Parser.php
@@ -1,5 +1,6 @@
 <?php namespace Vinelab\Youtube;
 
+use Vinelab\Youtube\Contracts\PlaylistInterface;
 use Vinelab\Youtube\Contracts\VideoInterface;
 use Vinelab\Youtube\Contracts\ChannelInterface;
 use Vinelab\Youtube\Contracts\ParserInterface;
@@ -20,22 +21,50 @@ class Parser implements ParserInterface {
 
     /**
      * Create a new Parser instance
-     * @param VideoInterface   $video
-     * @param ChannelInterface $channel
+     *
+     * @param \Vinelab\Youtube\Contracts\VideoInterface    $video
+     * @param \Vinelab\Youtube\Contracts\ChannelInterface  $channel
+     * @param \Vinelab\Youtube\Contracts\PlaylistInterface $playlist
      */
-    public function __construct(VideoInterface $video, ChannelInterface $channel)
-    {
+    public function __construct(
+        VideoInterface $video,
+        ChannelInterface $channel,
+        PlaylistInterface $playlist
+    ) {
         $this->video = $video;
         $this->channel = $channel;
+        $this->playlist = $playlist;
+    }
+
+    /**
+     * Parse the given video and channel pr playlist data.
+     *
+     * @param  array                   $video_pages
+     * @param  array                   $kind
+     *
+     * @return Vinelab\Youtube\Channel|Vinelab\Youtube\Playlist
+     */
+    public function parse($video_pages, $kind, $type = 'channel')
+    {
+        if($type == 'channel'){
+            return $this->parseChannel($video_pages, $kind);
+        }
+        if($type == 'playlist'){
+            return $this->parsePlaylist($video_pages, $kind);
+        }
+
+        return null;
     }
 
     /**
      * Parse the given video and channel data.
+     *
      * @param  array                   $video_pages
      * @param  array                   $channel
+     *
      * @return Vinelab\Youtube\Channel
      */
-    public function parse($video_pages, $channel)
+    public function parseChannel($video_pages, $channel)
     {
         $videos = new VideoCollection();
         //loop through the pages return from the api call
@@ -49,4 +78,28 @@ class Parser implements ParserInterface {
         //return a new channel with the new data.
         return $this->channel->make($channel, $videos);
     }
+
+    /**
+     * Parse the given video and playlist data.
+     *
+     * @param  array                   $video_pages
+     * @param  array                   $playlist
+     *
+     * @return Vinelab\Youtube\Playlist
+     */
+    public function parsePlaylist($video_pages, $playlist)
+    {
+        $videos = new VideoCollection();
+        //loop through the pages return from the api call
+        //and add all the video to the Video Collection to
+        //be passed to the 'channel make method'.
+        foreach ($video_pages as $page) {
+            foreach ($page->items as $video) {
+                $videos->push($this->video->make($video));
+            }
+        }
+        //return a new channel with the new data.
+        return $this->playlist->make($playlist, $videos);
+    }
+
 }
